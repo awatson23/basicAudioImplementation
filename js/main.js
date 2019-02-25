f4d9289aa3fbedd0ad4b5773b9a0f3a19a0cddfd
unknown <awatson23>
2019-02-25 12:49:09 -0500

    adding new content

---
+++ js/main.js
@@ -0,0 +1,38 @@
+(() => {
+	let aud = document.querySelector('audio'),
+		play = document.querySelector("#playButton"),
+		pause = document.querySelector("#pauseButton"),
+		rewind= document.querySelector("#rewindButton"),
+		tracks = document.querySelectorAll('.trackholder');
+
+	function playAudio() {
+		aud.play();
+	}
+
+	function pauseAudio() {
+		aud.pause(true);
+	}
+
+	function rewindAudio() {
+		aud.currentTime = 0,
+		
+		playAudio();
+	}
+
+	function switchAudioTrack() {
+		currentTrack = this.dataset.currentTrack;
+
+		aud.src = `audio/${currentTrack}`;
+		aud.load();
+		aud.play();
+	}
+
+	play.addEventListener("click", playAudio);
+	pause.addEventListener("click", pauseAudio);
+	rewind.addEventListener("click", rewindAudio);
+
+	tracks.forEach(track => {
+		track.addEventListener('click', switchAudioTrack);
+	});
+
+})();
